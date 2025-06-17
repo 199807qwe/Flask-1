9416e3e96d7af5a30b82cc6cb6035931e20b1134
Ivan Gulin <19980721qwe@gmail.com>
2025-06-17 10:46:40 +0300

    Add qoutes URL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -8,14 +8,44 @@ about_me = {
     "email": "19980721qwe@gmail.com",
 }
 
+quotes = [
+    {
+        "id": 3,
+        "author": "Rick Cook",
+        "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+    },
+    {
+        "id": 5,
+        "author": "Waldi Ravens",
+        "text": "Программирование на С похоже на быстрые танцына только что отполированном полу людей с острыми бритвами вруках."
+    },
+    {
+        "id": 6,
+        "author": "Mosher’s Law of Software Engineering",
+        "text": "Не волнуйтесь, если что-то не работает. Еслибы всё работало, вас бы уволили."
+    },
+    {
+        "id": 8,
+        "author": "Yoggi Berra",
+        "text": "В теории, теория и практика неразделимы. Напрактике это не так."
+    },
+]
+# Нужно больше цитат? https://tproger.ru/devnull/programming-quotes/
+
 @app.route("/") # Это первый УРЛ, который будет обрабатывать
 def hello_world(): # Эта функция-обработчик будет вызвана при запросе УРЛа.
-    return "Hello, World!!!"
+    return jsonify(data="Hello, World!!!")
 
 
 @app.route("/about")
 def about():
     return about_me
+
+# URL: /quotes
+@app.route("/quotes")
+def get_quotes() -> list[dict[str, any]]:
+        """Функция неявно преобразовывает список словарей в JSON"""
+        return quotes
 
 if __name__ == "__main__":
     app.run(debug=True)
